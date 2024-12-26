99f8bd4a03223a77cf11ca35b9a298f4772bf331
Samuel T. Fatunmbi <samueltaiwofatunmbi1@gmail.com>
2024-12-26 17:31:12 +0100

    Add project files.

---
+++ UserAPIs/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace UserAPIs.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+
+    DataContextDapper _dapper;
+    public UserController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+    }
+
+    [HttpGet("TestDbConnection")]
+    public DateTime TestData()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    }
+
+
+    [HttpGet("GetUsers/{stringValue}")]
+    public string[] GetUsers(string stringValue)
+    {
+        string[] stringResponse = new string[]{
+            "test one", "test two", stringValue
+        };
+        return stringResponse;
+    }
+}
---
+++ UserAPIs/Data/DataContextDapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace UserAPIs
+{
+    class DataContextDapper
+    {
+        private readonly IConfiguration _config;
+
+        public DataContextDapper(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IEnumerable<T> LoadData<T>(string sql)
+        {
+            IDbConnection dbconnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbconnection.Query<T>(sql);
+        }
+
+        public T LoadDataSingle<T>(string sql)
+        {
+            IDbConnection dbconnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbconnection.QuerySingle<T>(sql);
+        }
+    }
+}
